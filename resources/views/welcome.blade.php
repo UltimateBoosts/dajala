b8855657df70e739418398960517255a017cfe0c
Cristian Arrieta <cristian.arrieta@uberflug.co>
2019-05-28 22:42:51 -0500

    Landing construccion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,25 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-    </head>
-    <body>
-        <div id="example"></div>
-    </body>
-    <script src="{{ asset('js/app.js') }}" ></script>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Sitio en construcción</title>
+</head>
+
+<body>
+    <section class="construccion">
+        <img src="{{asset('images/fondo.jpg')}}" alt="">
+        <div class="container">
+            <div class="card">
+                <h1>Dajala</h1>
+                <h2>Cerros y Cordilleras</h2>
+                <h3>Sitio en construcción</h3>
+            </div>
+        </div>
+    </section>
+</body>
+
 </html>
