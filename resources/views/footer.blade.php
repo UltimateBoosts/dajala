d2d47fc99be5c876263293ffe89d3df75f962abe
Cristian Arrieta <darktowerdev@gmail.com>
2019-12-30 13:29:38 -0500

    Adjust

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,8 +5,8 @@
                 <a target="_blanc" href="https://www.facebook.com/Dajala-Cerros-y-Cordilleras-340595536860183">
                     <li class="list-inline-item"><span class="fab fa-facebook-f"></span>DajalaCerrosyCordilleras</li>
                 </a>
-                <a href="#">
-                    <li class="list-inline-item"><span class="fab fa-twitter"></span>DajalaCerrosyCordilleras</li>
+                <a target="_blanc" href="https://twitter.com/DajalaY">
+                    <li class="list-inline-item"><span class="fab fa-twitter"></span>@DajalaY</li>
                 </a>
                 <a href="mailto:info@dajalacerrosycordilleras.com">
                     <li class="list-inline-item"><span class="fas fa-envelope"> </span>info@dajalacerrosycordilleras.com</li>
